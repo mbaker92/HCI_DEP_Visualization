b67de2d73e04c7ced26aca37029998ad71017fb6
Matthew Baker <hackmattr@gmail.com>
2017-04-18 19:24:34 -0700

    Inital Commit.
    Originally on Microsoft Team Services for Version Control. Uploaded to
    Github April 2017. Original Code from December 2015

---
+++ DEPVisualization/DEPVisualization/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DEPVisualization._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <h1>DEP Visualization</h1>
+    </div>
+    
+    <!--  TODO: Add input boxes -->
+    <asp:Label runat="server" ID="ExampleLabel"></asp:Label>
+
+    <asp:button name="SubmitButton" Text="Submit" runat="server" onclick="SubmitButtonOnClick"></asp:button>
+    
+    <!--  TODO: Add charts (use nuget to get DotNetHighCharts  -->
+
+</asp:Content>
---
+++ DEPVisualization/DEPVisualization/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DEPVisualization
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void SubmitButtonOnClick(object sender, EventArgs e)
+        {
+            //((Label)this.Controls["ExampleLabel"]).Text = "You clicked a thing."; //This doesn't work, but should? I'll look at it later -Ed
+
+            //TODO: Verify input
+
+
+            //TODO: Calculate DEP graph
+
+
+            //TODO: plot graph using highcharts
+        }
+
+    }
+}
